74f61049973ea67fbea0d6e0f8c3afafb92512e2
Chazz Gagliano <cgags17285@gmail.com>
2023-06-05 13:10:27 -0400

    create favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_170718) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_170911) do
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
