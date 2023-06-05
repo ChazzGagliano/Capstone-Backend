e3394fa351e3609ba675c872445159911991bac4
Chazz Gagliano <cgags17285@gmail.com>
2023-06-05 15:09:44 -0400

    create user show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   post "/favorites" => "favorites#create"
   get "/favorites" => "favorites#index"
+  get "/users/:id" => "users#show"
   get "/users" => "users#index"
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   get "/movies" => "movies#index"
   post "/movie" => "movies#create"
   delete "/favorite/:id" => "favorites#destroy"
+
 end
