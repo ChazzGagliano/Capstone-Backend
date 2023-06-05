2e479703e3066fa4e633d27876b2ac1b7bb19fcc
Chazz Gagliano <cgags17285@gmail.com>
2023-06-05 14:25:47 -0400

    add movie create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,16 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     render :index
   end
+
+  def create
+    @movie = Movie.new(
+    name: params[:name],
+    image_url: params[:image_url],
+    description: params[:description],
+    category: params[:category]
+    )
+    @movie.save
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   get "/movies" => "movies#index"
+  post "/movie" => "movies#create"
 end
