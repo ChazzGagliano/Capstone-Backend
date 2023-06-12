28e9111562003d269cf086e6a467f6308959d567
Chazz Gagliano <cgags17285@gmail.com>
2023-06-12 14:31:18 -0400

    adjust backend api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,23 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    @actors = []
+    @movies.each do |movie|
+      url = URI("https://imdb8.p.rapidapi.com/auto-complete?q=#{movie.name}")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(url)
+      request["X-RapidAPI-Key"] = Rails.application.credentials.fetch(:movies_api)[:api_key]
+      request["X-RapidAPI-Host"] = 'imdb8.p.  rapidapi.com'
+
+      response = http.request(request)
+      data = JSON.parse(response.read_body)
+      actors = data["d"].map { |entry| entry["s"] } 
+      @actors.push(actors[0])
+    end
+    p @actors
     render :index
   end
 
--- app/views/movies/_movie.json.jbuilder
+++ app/views/movies/_movie.json.jbuilder
@@ -4,3 +4,4 @@ json.image_url movie.image_url
 json.description movie.description
 json.categroy movie.category 
 json.comments movie.comments
+json.actors @actors[@movies.find_index(movie)]
